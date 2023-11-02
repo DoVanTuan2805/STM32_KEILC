f0af792300f2bb4ce3e70d40dcc6c8baab6cba63
Do Van Tuan <108563552+Tuan285@users.noreply.github.com>
2023-11-02 16:10:25 +0700

    commit

---
+++ VL53L0X/MDK-ARM/VL53L0X/vl53l0x_api_calibration.d
@@ -0,0 +1,36 @@
+vl53l0x/vl53l0x_api_calibration.o: App\vl53l0x\vl53l0x_api_calibration.c \
+  App\vl53l0x\vl53l0x_api.h App\vl53l0x\vl53l0x_api_strings.h \
+  App\vl53l0x\vl53l0x_def.h App\vl53l0x\vl53l0x_device.h \
+  App\vl53l0x\vl53l0x_types.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stddef.h \
+  App\vl53l0x\vl53l0x_platform.h App\vl53l0x\vl53l0x_platform_log.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdio.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\string.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal.h \
+  ..\Core\Inc\stm32f1xx_hal_conf.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_rcc.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_def.h \
+  ..\Drivers\CMSIS\Device\ST\STM32F1xx\Include\stm32f1xx.h \
+  ..\Drivers\CMSIS\Device\ST\STM32F1xx\Include\stm32f103xb.h \
+  ..\Drivers\CMSIS\Include\core_cm3.h \
+  ..\Drivers\CMSIS\Include\cmsis_version.h \
+  ..\Drivers\CMSIS\Include\cmsis_compiler.h \
+  ..\Drivers\CMSIS\Include\cmsis_armclang.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_compat.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_acle.h \
+  ..\Drivers\CMSIS\Device\ST\STM32F1xx\Include\system_stm32f1xx.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\Legacy\stm32_hal_legacy.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_rcc_ex.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_gpio.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_gpio_ex.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_exti.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_dma.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_dma_ex.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_cortex.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_flash.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_flash_ex.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_i2c.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_pwr.h \
+  App\vl53l0x\vl53l0x_api_core.h App\vl53l0x\vl53l0x_api_calibration.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdlib.h
---
+++ VL53L0X/MDK-ARM/VL53L0X/vl53l0x_platform_log.d
@@ -0,0 +1,9 @@
+vl53l0x/vl53l0x_platform_log.o: App\vl53l0x\vl53l0x_platform_log.c \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdio.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdarg.h \
+  App\vl53l0x\vl53l0x_def.h App\vl53l0x\vl53l0x_device.h \
+  App\vl53l0x\vl53l0x_types.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stddef.h \
+  App\vl53l0x\vl53l0x_platform_log.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\string.h
